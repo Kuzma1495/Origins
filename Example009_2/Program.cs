5ac10eac59066e0498bf93c9cae756f989904bba
Kuzma <kuzmalebedev@yandex.ru>
2022-12-18 13:22:24 +0400

    Example009

---
+++ Example009_2/Program.cs
@@ -0,0 +1,16 @@
+﻿ int[] array = {17,2,63,44,5,4,75,4,9 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
