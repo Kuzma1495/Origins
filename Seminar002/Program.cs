c3b9632961824c5add0823de2e16ae2a1b24dbe8
Kuzma <kuzmalebedev@yandex.ru>
2022-12-18 11:44:30 +0400

    Seminar002

---
+++ Seminar002/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+
+int n = new Random().Next(10,100);
+int n1 = n / 10;
+int n2 = n % 10;
+Console.WriteLine(n);
+
+if (n1 > n2)
+    Console.WriteLine(n1);
+else if (n1 < n2)
+    Console.WriteLine(n2);
+else
+    Console.WriteLine("=");
