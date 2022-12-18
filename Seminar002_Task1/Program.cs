efe3c65b3a8d678ceb5f68e8124363695cbb26c5
Kuzma <kuzmalebedev@yandex.ru>
2022-12-18 11:55:58 +0400

    Seminar002_Task1

---
+++ Seminar002_Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, 
+//которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+
+int n = new Random().Next(100, 1000);
+int n1 = n / 100;
+int n3 = n % 10;
+Console.WriteLine(n);
+
+Console.WriteLine(n1*10 + n3);
