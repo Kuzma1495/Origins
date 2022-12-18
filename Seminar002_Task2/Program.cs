daae0f43033ce1401440640eb4734b8bf811d549
Kuzma <kuzmalebedev@yandex.ru>
2022-12-18 12:06:56 +0400

    Seminar002_Task2

---
+++ Seminar002_Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a % b == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine($"Остаток {a % b}");
