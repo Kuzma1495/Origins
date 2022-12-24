a31e5808a83f20cb981df873bae0d47e96fff77c
Kuzma <kuzmalebedev@yandex.ru>
2022-12-24 11:38:50 +0400

    Seminar003_Task2

---
+++ Seminar003_Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Clear();
+Console.Write("Четверть");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if (a == 1)
+    Console.WriteLine("x > 0 && y > 0");
+else if (a == 2)
+    Console.WriteLine("x < 0 && y > 0");
+else if (a == 3)
+    Console.WriteLine("x < 0 && y < 0");
+else if (a == 4)
+    Console.WriteLine("x > 0 && y < 0");
+else
+    Console.WriteLine("Введите число от 1 до 4");
