8675f1c48853a2f4a137c68131e46aee124a491e
Kuzma <kuzmalebedev@yandex.ru>
2022-12-24 11:28:41 +0400

    Seminar003_Task1

---
+++ Seminar003_Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №17. 
+// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+Console.Clear();
+Console.Write("Координату x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Координату y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
