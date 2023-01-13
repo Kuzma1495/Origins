d4c6830ab622068fc4328e819657670908a031e5
Kuzma <kuzmalebedev@yandex.ru>
2023-01-14 00:06:41 +0400

    MethodsReplace

---
+++ MethodTextReplace/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string Text = "Hello, my dudes";
+
+string ReplaceText(string Text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = Text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(Text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{Text[i]}";
+    }
+
+
+    return result;
+}
+string newText = ReplaceText(Text, ' ', '-');
+Console.WriteLine(newText);
