b27e7eab06ff5d998e6c1797717ba9b488d03c27
Kuzma <kuzmalebedev@yandex.ru>
2023-01-14 00:34:14 +0400

    MethodsReplaceArr

---
+++ MethodsArrayReplace/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] arr = {2, 3, 1, 6, 3, 7, 1, 2};
+
+void printArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+printArray(arr);
+SelectionSort(arr);
+
+printArray(arr);
