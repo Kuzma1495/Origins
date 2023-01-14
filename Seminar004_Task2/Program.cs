6a1663ace82226700eb31397bb331d93dfe1a6b6
Kuzma <kuzmalebedev@yandex.ru>
2023-01-14 12:17:09 +0400

    Seminar004_2

---
+++ Seminar004_Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 28: Напишите программу, которая принимает 
+//на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for (int i = 2; i <= n; i++)
+    res *= i;
+Console.Write(res);
+
