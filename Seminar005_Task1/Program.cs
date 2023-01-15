6d24dfe653506a41537224439d52666770e10ef5
Kuzma <kuzmalebedev@yandex.ru>
2023-01-15 11:31:52 +0400

    Seminar005

---
+++ Seminar005_Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void input_array(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10); // [-9;9]
+}
+
+void release(int[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+        array[i] *= -1;
+    
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+release(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
