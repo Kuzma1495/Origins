4e9366a9d09fafbac8ad5f01cf7bd19f205c447f
Kuzma <kuzmalebedev@yandex.ru>
2023-01-15 12:25:42 +0400

    Seminar005_Task4

---
+++ Seminar005_Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+//  Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 9
+// [6 7 3 6] -> 36 21
+
+
+
+void input_array(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11); 
+}
+
+
+void release(int[] array)
+{
+    for (int i = 0; i < array.Length/2 + array.Length % 2; i++)
+        Console.Write($"{array[i] * array[array.Length - 1 - i]} ");
+        
+    //[1, 2 , 3, 4 , 5]
+    
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+release(array);
