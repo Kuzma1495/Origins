d0aa44257c6640902c0c37b05389c21fd1723d18
Kuzma <kuzmalebedev@yandex.ru>
2023-01-21 15:37:56 +0400

    Task1

---
+++ Seminar006_Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// // **Задача 40:** Напишите программу, которая принимает 
+//на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+// ❗ **Теорема о неравенстве треугольника:** каждая сторона 
+//треугольника меньше суммы двух других сторон.
+
+
+void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11); // [1, 10]
+}
+
+string Triangle(int[] array)
+{
+    if(array[0] < array[1] + array[2] &&
+        array[1] < array[0] + array[2] &&
+        array[2] < array[0] + array[1])
+        return "yes";
+    return "no";
+}
+
+
+Console.Clear();
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine(Triangle(array));
+
