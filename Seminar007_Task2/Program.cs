b4853476b12ba2e3e5fe715218ce516e273961f7
Kuzma <kuzmalebedev@yandex.ru>
2023-01-22 11:57:39 +0400

    Task2

---
+++ Seminar007_Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+//находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int Release(int[,] matrix){
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+                sum = sum + matrix[i,j];
+            
+        }
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальная матрица: ");
+InputMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Сумма ");
+Console.WriteLine(Release(matrix));
